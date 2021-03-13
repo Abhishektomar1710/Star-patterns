d839c4ba5ccdffcadeac36614b366e6d71ddf417
ABHISHEK TOMAR <74098870+Abhishektomar1710@users.noreply.github.com>
2021-03-13 18:16:59 +0530

    Add files via upload

---
+++ diamond shape.c
@@ -0,0 +1,45 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    for(int i=1;i<=4;i++)
+    {
+        for(int j=4;j>i;j--)
+        {
+            printf(" ");
+        }
+        for(int k=1;k<=i;k++)
+        {
+            printf("*");
+        }
+        for(int l=2;l<=i;l++)
+        {
+            printf("*");
+        }
+         printf("\n");
+    }
+
+   for(int i=1;i<=3;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            printf(" ");
+        }
+
+        for(int j=3;j>=i;j--)
+        {
+            printf("*");
+        }
+
+        for(int j=3;j>i;j--)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+}
+
+}
+
+
